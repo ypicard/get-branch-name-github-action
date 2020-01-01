567451720d9b8e2843c754a034dac02f25af57cf
Aaron Roller <aaron.roller@automodality.com>
2020-01-01 10:49:00 -0800

    feat: Added a check if pull request then use a git command to discover the current branch.  Using docker image with git on it.  Avoiding breaking changes by keeping same behavior if git checkout is not used 1-pr-branch

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 # Container image that runs your code
-FROM alpine:3.10
+FROM alpine/git:1.0.7
 RUN apk add --no-cache --upgrade bash
 
 # Copies your code file from your action repository to the filesystem path `/` of the container
--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,15 @@ TAGS_MAP=( ["master"]="latest" ["development"]="development")
 BRANCH=$(echo ${GITHUB_REF} | sed -e "s/refs\/heads\///g")
 TAG=${TAGS_MAP[$BRANCH]}
 
+# if contains /refs/pull/ for pull requests
+if [ $(echo ${GITHUB_REF} | sed -e "s/refs\/pull\///g") != ${GITHUB_REF} ]; then
+  cd /github/workspace 
+  #only call git if checkout was done
+  if [ -d .git ]; then
+    BRANCH=$(git rev-parse --abbrev-ref HEAD)
+  fi;
+fi;
+
 # if contains /refs/tags/
 if [ $(echo ${GITHUB_REF} | sed -e "s/refs\/tags\///g") != ${GITHUB_REF} ]; then
   TAG="latest"
